bb467c470d39b5d7caee8a27a478c35fa42769ac
Bennett Hansen <bennett.hansen@gmail.com>
2013-04-12 14:58:20 -0500

    Checks all possible combinations of starting moves

--- tictactoe.py
+++ tictactoe.py
@@ -1,47 +1,64 @@
-oddMoves = set([1,3,5,7,9])
-evenMoves = set([2,4,6,8])
-board = [0]*9
-
 def oddTurn(board, oddMoves, evenMoves):
     """return True means odd has a winning strategy"""
     for i in range(9):
-        if board[i]==0:
+        if board[i]==-100:
             for j in oddMoves:
                 newboard = board[:]
                 newboard[i] = j
-                print newboard
+                #print newboard
                 if isGameOver(newboard)==1:
                     return True
                 elif isGameOver(newboard)==2:
                     return False
                 newoddMoves = oddMoves-set([j])
                 newevenMoves = evenMoves.copy()
-                if evenTurn(newboard, newoddMoves, newevenMoves) is False:
-                    return False
-    return True
+                if evenTurn(newboard, newoddMoves, newevenMoves) is True:
+                    return True
+    return False
 
 def evenTurn(board, oddMoves, evenMoves):
     """return True means odd wins no matter what even move"""
     for i in range(9):
-        if board[i]==0:
+        if board[i]==-100:
             for j in evenMoves:
                 newboard = board[:]
                 newboard[i] = j
-                print newboard
+                #print newboard
                 if isGameOver(newboard)==1:
                     return False
                 newoddMoves = oddMoves.copy()
                 newevenMoves = evenMoves-set([j])
-                if oddTurn(newboard, newoddMoves, newevenMoves) is True:
-                    return True
-    return False
+                if oddTurn(newboard, newoddMoves, newevenMoves) is False:
+                    return False
+    return True
 
 def isGameOver(board):
     """return 0 for unfinished, 1 for win, 2 for tie"""
-    if board[0]+board[1]+board[2]==15 or board[3]+board[4]+board[5]==15 or board[6]+board[7]+board[8]==15 or board[0]+board[3]+board[6]==15 or board[1]+board[4]+board[7]==15 or board[2]+board[5]+board[8]==15 or board[0]+board[4]+board[8]==15 or board[2]+board[4]+board[6]==15:
+    global games
+    games += 1
+    if sum(board[0::3])==15 or sum(board[1::3])==15 or sum(board[2::3])==15 or sum(board[0:3])==15 or sum(board[3:6])==15 or sum(board[6:9])==15 or sum(board[0::4])==15 or sum(board[2:7:2])==15:
+        #print board
         return 1
-    if board[0]!=0 and board[1]!=0 and board[2]!=0 and board[3]!=0 and board[4]!=0 and board[5]!=0 and board[6]!=0 and board[7]!=0 and board[8]!=0:
+    if sum(board)>0:
         return 2
     return 0
 
-oddTurn(board, oddMoves, evenMoves)
+import itertools
+
+#oddMoves = set([1,3,5,7,9])
+#evenMoves = set([2,4,6,8])
+#board = [0]*9
+
+moves = set(range(1,10))
+for i in itertools.combinations(range(1,10), 5):
+    games = 0
+    p1Moves = set(i)
+    p2Moves = moves - p1Moves
+    board = [-100]*9
+    oddWin = oddTurn(board, p1Moves, p2Moves)
+    if oddWin is False:
+        print "BIG PROBLEM"
+        break
+    print p1Moves, p2Moves, oddWin, games
+
+#print oddTurn(board, oddMoves, evenMoves)
